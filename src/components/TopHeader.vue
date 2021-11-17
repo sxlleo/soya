0c00cc565f60c02247e38eeda2bf0c7acb39b964
songxiaolin <songxiaolin@aixuexi.com>
2021-11-17 17:44:37 +0800

    upd: 1

---
+++ src/components/TopHeader.vue
@@ -0,0 +1,137 @@
+<template>
+  <div :class="['top-header', `style${theme}`]">
+    <div class="content">
+      <div class="logo">
+        <router-link to="home">
+          <img class="btn" :src='logoUrl[theme-1]' alt="">
+        </router-link>
+      </div>
+      <p class="works"><router-link to="works" class="btn">WORKS</router-link></p>
+      <p class="about"><router-link to="about" class="btn">ABOUT</router-link></p>
+      <a class="contant btn" href="mailto:yangfan@soyastudio.net">CONTANT US</a>
+      <!-- <div class="link-container">
+        <p class="work-container"><router-link to="works" class="btn">WORKS</router-link></p>
+        <p class="about-container"><router-link to="about" class="btn">ABOUT</router-link></p>
+        <a class="contant btn" href="mailto:yangfan@soyastudio.net">CONTANT US</a>
+      </div> -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "TopHeader",
+    props: {
+      // 1: 黑字白底，2：白字黑底
+      theme: {
+        type: Number,
+        default: 1
+      }
+    },
+    data() {
+      return {
+        logoUrl:[
+          require('../assets/images/about/logo.png'),
+          require('../assets/images/works/logo.png'),
+        ]
+      }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+.flex {
+  display: flex;
+  align-items: flex-end;
+}
+.top-header {
+  font-family: 'Montserrat';
+  line-height: 200px;
+  font-size: 20px;
+  font-weight: bold;
+  background-color: #000;
+  padding: 4% 0 3%;
+  box-sizing: border-box;
+  color: #fff;
+  align-items: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-bottom: 1px solid;
+  &.style1 {
+    border-bottom-color: #000;
+  }
+  &.style2 {
+    border-bottom-color: #fff;
+  }
+  .content {
+    width: 100%;
+    justify-content: flex-start;
+    .flex();
+    .btn {
+      line-height: 100%;
+      cursor: pointer;
+      text-decoration:none;
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+    .logo{
+      width: 15%;
+      min-width: 15%;
+      margin-left: 4%;
+      &, & > * {
+        display: inline-flex;
+      }
+      img {
+        width: 120px;
+      }
+    }
+    .works, .about {
+      display: inline-flex;
+      width: 15.1%;
+    }
+    .contant {
+      position: absolute;
+      right: 4.9%;
+    }
+    // .link-container {
+    //   // display: flex;
+    //   // justify-content: space-between;
+    //   display: inline-flex;
+    //   width: 100%;
+    //   .work-container {
+    //     width: 18.9%;
+    //   }
+    //   p {
+    //     display: inline-flex;
+    //   }
+    //   .contant {
+    //     position: absolute;
+    //     right: 4.8%;
+    //     text-decoration: none;
+    //     white-space: nowrap;
+    //     margin: 0 !important;
+    //     // text-align: right;
+    //   }
+    // }
+  }
+  .left {
+    width: 40%;
+    justify-content: space-between;
+    .flex();
+  }
+}
+.style1 {
+  background-color: #fff;
+  .btn{
+    color: #000;
+  }
+}
+.style2{
+  background-color: #000;
+  .btn{
+    color: #fff;
+  }
+}
+</style>
---
+++ src/components/WorkItem.vue
@@ -0,0 +1,99 @@
+<template>
+  <div
+    class="work-item"
+    :key="`gif${gifIndex}`"
+    @mouseenter="onMouseenter"
+    @mouseleave="onMouseleave"
+    @click="() => {onClickWork(gif.zcoolUrl)}"
+  >
+    <div>
+      <div v-if="!loadResult" :style="{'margin-top': `${100 * 655 / 1166}%`}"></div>
+      <img v-show="loadResult" :src="gif.card" alt lazyload="on" @load="onLoad"/>
+      <img
+        v-if="hoverIndex == `${rowIndex}/${gifIndex}`"
+        :class="['hover', {show: hoverIndex == `${rowIndex}/${gifIndex}`}]"
+        :src="gif.hover"
+        alt
+      />
+    </div>
+    <p class="title">{{gif.title}}</p>
+    <p class="des">{{gif.des}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "WorkItem",
+  props: {
+    gif: {
+      type: Object,
+      default: () => ({})
+    },
+    gifIndex: {
+      type: Number,
+      default: 0
+    },
+    hoverIndex: {
+      type: String,
+      default: ""
+    },
+    rowIndex: {
+      type: Number,
+      default: 0
+    }
+  },
+  data() {
+    return {
+      loadResult: false
+    }
+  },
+  methods: {
+    onLoad() {
+      this.loadResult = true;
+    },
+    onClickWork(url) {
+      window.open(url, "_blank");
+    },
+    onMouseenter() {
+      this.$emit("mouseenter")
+    },
+    onMouseleave() {
+      this.$emit("mouseleave")
+    }
+  }
+}
+</script>
+
+<style lang='less' scoped>
+.work-item {
+  position: relative;
+  width: 32.5%;
+  cursor: pointer;
+  img {
+    width: 100%;
+  }
+  .hover {
+    position: absolute;
+    top: 0;
+    left: 0;
+    opacity: 1;
+    transition: 200ms ease-out;
+  }
+  p {
+    text-align: left;
+  }
+  .title {
+    font-size: 20px;
+    font-family: Source Han Sans CN;
+    font-weight: bold;
+    margin-bottom: 10px;
+  }
+  .des {
+    font-size: 14px;
+    font-family: "Montserrat";
+    font-weight: 500;
+    line-height: 0px;
+    color: #9b9b9b;
+  }
+}
+</style>
